21c93f4bee1f38a9795560df89600cb8b81a6ef6
Julian Buchholz <github@julianbuchholz.com>
2021-11-25 16:20:48 +0100

    Change function name

--- src/app/pages/MapView.tsx
+++ src/app/pages/MapView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LocationMap from '../components/Map/Map';
+import LocationMap from '../components/LocationMap/LocationMap';
 
 export default function MapView() {
   return <LocationMap />;
