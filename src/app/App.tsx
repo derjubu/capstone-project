b9b4512d51c4aafb8ced4809befb189b3b4c1c6f
Julian Buchholz <github@julianbuchholz.com>
2021-11-25 08:50:40 +0100

    Remove states from App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,49 +5,13 @@ import AddDestination from './pages/AddDestination/AddDestination';
 import DestinationDetailView from './pages/DestinationDetailView/DestinationDetailView';
 
 function App(): JSX.Element {
-  const [activities, setActivities] = useState(['Climbing', 'Walking']);
-  const [location, setLocation] = useState('');
-  const [startTrip, setStartTrip] = useState('');
-  const [endTrip, setEndTrip] = useState('');
-
-  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    console.log(location, startTrip, endTrip, activities);
-    setLocation('');
-    setStartTrip('');
-    setEndTrip('');
-    setActivities([]);
-  }
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <AddDestination
-              activities={activities}
-              setActivities={setActivities}
-              location={location}
-              setLocation={setLocation}
-              startTrip={startTrip}
-              setStartTrip={setStartTrip}
-              endTrip={endTrip}
-              setEndTrip={setEndTrip}
-              onSubmit={onSubmit}
-            />
-          }
-        />
+        <Route path="/" element={<AddDestination />} />
         <Route
           path="/DestinationDetailView"
-          element={
-            <DestinationDetailView
-              activities={activities}
-              location={location}
-              startTrip={startTrip}
-              endTrip={endTrip}
-            />
-          }
+          element={<DestinationDetailView />}
         />
       </Routes>
     </BrowserRouter>
