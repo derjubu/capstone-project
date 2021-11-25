e671b39ddc0675e8a968172bb6272d03cd7ae568
Julian Buchholz <github@julianbuchholz.com>
2021-11-25 14:28:22 +0100

    Remove global.css from preview.tsx

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router';
 
-import '../src/app/globals.css';
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
