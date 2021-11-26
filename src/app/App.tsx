0a005fcde0ceb037687cdaab1e5eda6c073fc3bc
Julian Buchholz <github@julianbuchholz.com>
2021-11-26 06:55:21 +0100

    Declare Destination type and add destinations array

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //import { useNavigate } from 'react-router';
 import AddDestination from './pages/AddDestination/AddDestination';
 import DestinationDetailView from './pages/DestinationDetailView/DestinationDetailView';
 import MapView from './pages/MapView/MapView';
 
+type Destination = {
+  location: string;
+  startDate: string;
+  endDate: string;
+};
+
 function App(): JSX.Element {
+  const [destinations, setDestinations] = useState<Destination[]>([]);
+
   return (
     <BrowserRouter>
       <Routes>
