5da2aa99fd7dcde7da7a8bfccc36d1398a4dbd35
Julian Buchholz <github@julianbuchholz.com>
2021-11-26 07:49:30 +0100

    Fix smaller bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DestinationType } from './utils/DestinationType';
+import type { DestinationType } from './utils/DestinationType';
 import AddDestination from './pages/AddDestination/AddDestination';
 import DestinationDetailView from './pages/DestinationDetailView/DestinationDetailView';
 import MapView from './pages/MapView/MapView';
