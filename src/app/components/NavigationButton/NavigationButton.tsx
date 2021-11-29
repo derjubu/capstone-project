114be942285e20ebb95e0727dea70becfcc1a622
Julian Buchholz <github@julianbuchholz.com>
2021-11-29 16:48:07 +0100

    Remove unneccesary code from NavigationButton.tsx

--- src/app/components/NavigationButton/NavigationButton.tsx
+++ src/app/components/NavigationButton/NavigationButton.tsx
@@ -2,15 +2,13 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavigationButton = styled(Link)`
+  text-decoration: none;
   border: 2px var(--color-secondary) solid;
   border-radius: 5px;
   background: var(--color-background-secondary);
   color: var(--color-secondary);
   font-size: 1.5rem;
   font-weight: bold;
-  margin-top: 1rem;
-  margin-left: auto;
-  margin-right: auto;
   padding: 0 1rem;
   display: block;
   width: fit-content;
