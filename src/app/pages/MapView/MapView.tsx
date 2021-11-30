bb90b8e8a767027f77b2cf8a0e578c58611f7ac4
Julian Buchholz <github@julianbuchholz.com>
2021-11-30 19:17:29 +0100

    Fix bug on MapView Testpage

--- src/app/pages/MapView/MapView.tsx
+++ src/app/pages/MapView/MapView.tsx
@@ -2,5 +2,10 @@ import React from 'react';
 import MapWithMarker from '../../components/LocationMap/MapwithMarker';
 
 export default function MapView(): JSX.Element {
-  return <MapWithMarker />;
+  return (
+    <>
+      <div id="geocoderArea"></div>
+      <MapWithMarker displayArea={'#geocoderArea'} />
+    </>
+  );
 }
