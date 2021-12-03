10ecbc07f0449bdd99898daa31404762e46f007a
Julian Buchholz <github@julianbuchholz.com>
2021-12-03 18:04:16 +0100

    Fix import bug

--- src/app/components/ItineraryMap/ItineraryMap.tsx
+++ src/app/components/ItineraryMap/ItineraryMap.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
-import mapboxgl, { LngLatBoundsLike } from 'mapbox-gl';
-import type { Map, LngLatLike } from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
+import type { Map, LngLatLike, LngLatBoundsLike } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import styled from 'styled-components';
 import * as turf from '@turf/turf';
