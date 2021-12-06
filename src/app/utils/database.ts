b8fd90c6a327f23b857887530e9e9ad5a72fb562
Julian Buchholz <github@julianbuchholz.com>
2021-12-06 12:41:24 +0100

    Add getItinerary function

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -5,3 +5,7 @@ export async function connectDatabase(url: string) {
   client = new MongoClient(url);
   await client.connect();
 }
+
+export function getItinerary() {
+  return client.db().collection('destinations');
+}
