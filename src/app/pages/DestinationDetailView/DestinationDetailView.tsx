f931f40868530367fd80fdf60b1d44c2a7405f0a
Julian Buchholz <github@julianbuchholz.com>
2021-12-18 18:18:38 +0100

    Apply general styling

--- src/app/pages/DestinationDetailView/DestinationDetailView.tsx
+++ src/app/pages/DestinationDetailView/DestinationDetailView.tsx
@@ -3,6 +3,8 @@ import ButtonNavigate from '../../components/ButtonNavigate/ButtonNavigate';
 import DestinationCard from '../../components/DestinationCard/DestinationCard';
 import type { DestinationType } from '../../utils/DestinationType';
 import LocationMap from '../../components/LocationMap/LocationMap';
+import ButtonArea from '../../components/ButtonArea/ButtonArea';
+import CheckInputArea from '../../components/CheckInputArea/CheckInputArea';
 
 export default function DestinationDetailView(): JSX.Element {
   const currentDestination: DestinationType = JSON.parse(
@@ -50,16 +52,19 @@ export default function DestinationDetailView(): JSX.Element {
   }
 
   return (
-    <>
+    <CheckInputArea>
       <DestinationCard
         location={currentLocation}
         startDate={startDate}
         endDate={endDate}
       />
       <LocationMap longitude={longitude} latitude={latitude} />
-      <ButtonNavigate link="/" onClick={goToItinerary}>
-        Go on
-      </ButtonNavigate>
-    </>
+      <ButtonArea>
+        <ButtonNavigate link="/AddDestination">Back</ButtonNavigate>
+        <ButtonNavigate link="/" onClick={goToItinerary}>
+          Go on
+        </ButtonNavigate>
+      </ButtonArea>
+    </CheckInputArea>
   );
 }
